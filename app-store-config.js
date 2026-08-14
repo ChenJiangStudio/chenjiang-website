36f587ab7b0ceb64e5b10c19845a2183d08f8fe9
ChenJiangStudio <knprv65zy9@privaterelay.appleid.com>
2026-08-14 19:29:36 +0800

    fix: publish app store download and qr code

--- app-store-config.js
+++ app-store-config.js
@@ -1,5 +1,5 @@
 window.APP_STORE_CONFIG = {
-  appStoreUrl: '',
-  appStoreQrImageUrl: '',
-  appStoreAvailable: false
+  appStoreUrl: 'https://apps.apple.com/tw/app/%E5%AE%B8%E5%8C%A0%E6%9C%A8%E4%BD%9C%E5%B7%A5%E5%85%B7%E7%AE%B1/id6789951209',
+  appStoreQrImageUrl: 'https://chenjiangstudio.github.io/chenjiang-website/assets/app/chenjiang-app-store-qr.png',
+  appStoreAvailable: true
 };
